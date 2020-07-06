3570333a847dfad4c303f1d74e2f44365a68187c
Genaro Aguero <fixed-term.GenaroAntonio.AgueroGarcia@bcn.bosch.com>
2020-07-06 02:48:42 +0200

    Validations added to avoid crashing when DataCypher does not have proper length to decrypt.

--- SimpleEncrypt/Program.cs
+++ SimpleEncrypt/Program.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace com.mercantilbanco.api.sample
+﻿namespace com.mercantilbanco.api.sample
 {
-    static class Program
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Windows.Forms;
+
+    internal static class Program
     {
+        public static bool IsBase64String(this string s)
+        {
+            s = s.Trim();
+            return s.Length % 4 == 0 && Regex.IsMatch(s, @"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None);
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
--- SimpleEncrypt/Util.cs
+++ SimpleEncrypt/Util.cs
@@ -1,74 +1,115 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace com.mercantilbanco.api.sample
+﻿namespace com.mercantilbanco.api.sample
 {
-    class Util
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    internal class Util
     {
-        private byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
-        private int BlockSize = 128;
+        private readonly byte[] iv =
+        {
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16
+        };
+
         /// <summary>
         /// Metodo para cifrar datos en AES con bloques de 128
         /// </summary>
         /// <param name="secretkey">Clave utilizada para cifrar los datos</param>
         /// <param name="datatocypher">Datos para ser cifrados</param>
         /// <returns></returns>
-        public string Encrypt(String secretkey, string datatocypher)
-        {            
-            byte[] iv = new byte[16];
+        public string Encrypt(string secretkey, string datatocypher)
+        {
+            //var iv = new byte[16];
             byte[] array;
 
-            using (Aes aes = Aes.Create())
+            using (var advanceEncryptionStandard = Aes.Create())
             {
-                aes.Key = ComputeSha256Hash(secretkey); 
-                aes.IV = iv;
-                aes.Padding = PaddingMode.PKCS7;
-                aes.Mode =  CipherMode.ECB;
-                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-                using (MemoryStream memoryStream = new MemoryStream())
+                Debug.Assert(
+                    !(advanceEncryptionStandard is null),
+                    $"{nameof(advanceEncryptionStandard)} was null");
+                advanceEncryptionStandard.Key = ComputeSha256Hash(secretkey);
+                advanceEncryptionStandard.IV = iv;
+                advanceEncryptionStandard.Padding = PaddingMode.PKCS7;
+                advanceEncryptionStandard.Mode = CipherMode.ECB;
+                ICryptoTransform encryptor = advanceEncryptionStandard.CreateEncryptor(
+                    advanceEncryptionStandard.Key,
+                    advanceEncryptionStandard.IV);
+                using (var memoryStream = new MemoryStream())
                 {
-                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    using (var cryptoStream = new CryptoStream(
+                        memoryStream,
+                        encryptor,
+                        CryptoStreamMode.Write))
                     {
-                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        using (var streamWriter = new StreamWriter(cryptoStream))
                         {
                             streamWriter.Write(datatocypher);
                         }
+
                         array = memoryStream.ToArray();
                     }
                 }
             }
+
             return Convert.ToBase64String(array);
         }
+
         /// <summary>
         /// Metodo para descifrar datos en AES con bloques de 128
         /// </summary>
         /// <param name="secretkey">Clave utilizada para descifrar los datos</param>
         /// <param name="datatodecrpyt">Datos para ser descifrados</param>
         /// <returns></returns>
-
-        public string Decrypt(String secretkey, string datatodecrpyt)
+        public string Decrypt(string secretkey, string datatodecrpyt)
         {
-            byte[] iv = new byte[16];
+            if (!datatodecrpyt.IsBase64String() || !(datatodecrpyt.Length >= 4))
+            {
+                return datatodecrpyt;
+            }
+            //var iv = new byte[16];
             byte[] buffer = Convert.FromBase64String(datatodecrpyt);
 
-            using (Aes aes = Aes.Create())
+            if (!(buffer.Length >= 4))
             {
-                aes.Key = ComputeSha256Hash(secretkey);
-                aes.IV = iv;
-                aes.Padding = PaddingMode.PKCS7;
-                aes.Mode = CipherMode.ECB;
-                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                return datatodecrpyt;
+            }
 
-                using (MemoryStream memoryStream = new MemoryStream(buffer))
+            using (var advanceEncryptionStandard = Aes.Create())
+            {
+                Debug.Assert(
+                    !(advanceEncryptionStandard is null),
+                    $"{nameof(advanceEncryptionStandard)} was null");
+                advanceEncryptionStandard.Key = ComputeSha256Hash(secretkey);
+                advanceEncryptionStandard.IV = iv;
+                advanceEncryptionStandard.Padding = PaddingMode.PKCS7;
+                advanceEncryptionStandard.Mode = CipherMode.ECB;
+                ICryptoTransform decryptor = advanceEncryptionStandard.CreateDecryptor(
+                    advanceEncryptionStandard.Key,
+                    advanceEncryptionStandard.IV);
+
+                using (var memoryStream = new MemoryStream(buffer))
                 {
-                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    using (var cryptoStream =
+                        new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
                     {
-                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        using (var streamReader = new StreamReader(cryptoStream))
                         {
                             return streamReader.ReadToEnd();
                         }
@@ -77,15 +118,13 @@ namespace com.mercantilbanco.api.sample
             }
         }
 
-   
         private byte[] ComputeSha256Hash(string rawData)
         {
-            
-            using (SHA256 sha256Hash = SHA256.Create())
+            using (var sha256Hash = SHA256.Create())
             {
-                byte[] key = new byte[16];
-                byte[] hash= sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
-                Array.Copy(hash,key, 16);
+                byte[] key = iv;
+                byte[] hash = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                Array.Copy(hash, key, iv.Length);
                 return key;
             }
         }
